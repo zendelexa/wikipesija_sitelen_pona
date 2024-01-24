7b5254f96675691eeb8a700c698b6d71d8e82323
zendelexa <azscience735@gmail.com>
2024-01-24 14:41:39 +0500

    Bug fixes

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ function main() {
 	var original_document_body = document.body.cloneNode(true);
 
 	var modified_document_body = document.body.cloneNode(true);
-	changeToSitelenPona(modified_document_body);
 
 	// Add the toggle button to the original page
 	var button1 = document.createElement("button");
@@ -15,8 +14,24 @@ function main() {
 	button1.style = "margin-top:1em;margin-right:15px";
 	document.getElementById("right-navigation").insertAdjacentElement("beforeend", button1);
 
-
+	// Change the writing system
+	changeToSitelenPona(modified_document_body);
 	document.body = modified_document_body.cloneNode(true);
+	handleWeirdTexts(); // Some of them just can't behave
+	function handleWeirdTexts() {
+		// This text had a space character that somehow broke the isLetter function
+		var lipuwikipesija_textnode = document.querySelector('[title="lipu Wikipesija"]');
+		if (lipuwikipesija_textnode != null) {
+			lipuwikipesija_textnode.childNodes[0].innerText = "lipu Wikipesija";
+			changeToSitelenPona(lipuwikipesija_textnode);
+		}
+
+		// For some reason this text changed its writing system back
+		setTimeout(() => {
+			changeToSitelenPona(document.querySelector('[title="Edit this page [Alt+Shift+v]"]'));
+		}, 300);
+	}
+	modified_document_body = document.body.cloneNode(true);
 
 	// Add the toggle button to the modified page
 	var button2 = document.createElement("button");
@@ -27,8 +42,6 @@ function main() {
 		document.getElementById("right-navigation").insertAdjacentElement("beforeend", button1);
 	});
 	document.getElementById("right-navigation").insertAdjacentElement("beforeend", button2);
-
-	
 }
 
 main();
